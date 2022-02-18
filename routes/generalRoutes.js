b0600f0d2177c6d671dcfea271daa310741f6991
Aaryashivani <sundropdiamond@gmail.com>
2022-02-18 13:57:40 +0530

    Added dummy data to substituted for scraped articles

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -1,7 +1,9 @@
 const generalRoutes = require('express').Router();
 
 generalRoutes.get("/viewArticles", (req, res) => {
-    res.send("These are the articles");
+    const date = new Date();// for testing purposes
+    const scrapedArticles = [{ "field1": "Hello World1", "hours": date.getHours(), "minutes": date.getMinutes(), "seconds": date.getSeconds() }, { "field1": "Hello World2" }];
+    res.json(scrapedArticles);
 });
 
 module.exports = generalRoutes;
