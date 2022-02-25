83903c6baa58690af71d6abc41a185f5f8d1c33b
viola-star <sundropdiamond@gmail.com>
2022-02-25 14:31:49 +0530

    Created validator files for registration and login

---
+++ validation/loginValidation.js
@@ -0,0 +1,8 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+function validateLoginInput(data) {
+    
+}
+
+module.exports = validateLoginInput;
---
+++ validation/registerValidation.js
@@ -0,0 +1,8 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+function validateRegistrationInput(data) {
+    
+}
+
+module.exports = validateRegistrationInput;
