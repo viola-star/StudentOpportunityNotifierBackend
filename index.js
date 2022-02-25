767379fea4f41b37e9e744e2738843f8d48373e7
viola-star <sundropdiamond@gmail.com>
2022-02-25 13:43:10 +0530

    Set up environment variable for port

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");;
+const cors = require("cors");
+require('dotenv').config();
 
 const generalRoutes = require("./routes/generalRoutes.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors()); 
 app.use('/api', generalRoutes);
 
-const port = 5000;
+const port = process.env.port || 5000;
 
 app.listen(port, () => {
     console.log(`App running port ${port}`);
