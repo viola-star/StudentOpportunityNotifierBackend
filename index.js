30c4d1373c981fd6448081c5e29041b766f97e3c
viola-star <sundropdiamond@gmail.com>
2022-02-25 13:56:52 +0530

    Initialized mongodb connection using mongoose

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const cors = require("cors");
 require('dotenv').config();
 
 const generalRoutes = require("./routes/generalRoutes.js");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors()); 
-app.use('/api', generalRoutes);
+app.use(cors());
+app.use('/api', generalRoutes); 
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log("MongoDB connected successfully!")).catch((err) => console.log(err));
+ 
 const port = process.env.port || 5000;
 
 app.listen(port, () => {
